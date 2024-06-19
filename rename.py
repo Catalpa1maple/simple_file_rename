f898290e924ea0d088aa0600e0defec5f401c3b8
ParkerWong <113246594+Catalpa1maple@users.noreply.github.com>
2024-06-19 18:01:46 +0800

    Update rename.py

--- rename.py
+++ rename.py
@@ -1,15 +1,22 @@
 import os
 import shutil
 
-folder_path = './Audio_file'
+folder_path = input("folder's path?")
 
 filenames = os.listdir(folder_path)
 
-index = 0
+file_type = input("type of files?")
 
+index = 0
+i = input("Index?")
 for filename in filenames:
-    old = "{}.mp3".format(index)
-    new = "{}.mp3".format(index+2000)
-    os.rename(old,new)
+    old = folder_path+"/{}.{}".format(index,file_type)
+    new = folder_path+"/{}.{}".format((index+i),file_type)
+    try:
+        os.rename(old,new)
+    except:
+        pass
+    # print(old)
+    # print(new)
     index += 1
     
