180d6887419b085ebdb3c921e90108a1c4e2dfe2
ParkerWong <113246594+Catalpa1maple@users.noreply.github.com>
2024-06-20 11:02:45 +0800

    Update rename.py

--- rename.py
+++ rename.py
@@ -8,7 +8,7 @@ filenames = os.listdir(folder_path)
 file_type = input("type of files?")
 
 index = 0
-i = input("Index?")
+i = int(input("Index?"))
 for filename in filenames:
     old = folder_path+"/{}.{}".format(index,file_type)
     new = folder_path+"/{}.{}".format((index+i),file_type)
